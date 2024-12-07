dd0a8cbaebcf21c3fce45ac36fcb211efb87682d
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2024-12-07 18:52:44 +0200

    Add pagination to the view were we show all the books

--- eLibrayProjectDemo/Controllers/LibaryController.cs
+++ eLibrayProjectDemo/Controllers/LibaryController.cs
@@ -1,4 +1,5 @@
-﻿using eLibrayProjectDemo.Services;
+﻿using eLibrayProjectDemo.Models;
+using eLibrayProjectDemo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eLibrayProjectDemo.Controllers
@@ -6,18 +7,36 @@ namespace eLibrayProjectDemo.Controllers
     public class LibaryController : Controller
     {
         private readonly AppDbContext context;
+        private readonly int pageSize = 8;
 
         public LibaryController(AppDbContext context)
         {
             this.context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex)
         {
             //var books=context.Books.OrderByDescending(p=>p.EbookId).ToList();
 
             //ViewBag.Books=books;
 
-            var books = context.Books.OrderByDescending(p => p.EbookId).ToList();
+            IQueryable<Book>query=context.Books;
+            query = query.OrderByDescending(p => p.EbookId);
+
+            //pagination functionalty
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count=query.Count();
+            int totalPages=(int)Math.Ceiling(count/pageSize);
+            query=query.Skip((pageIndex-1)*pageSize).Take(pageSize);
+
+            var books = query.ToList();
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageIndex=pageIndex;
+            ViewBag.Books=books;
             return View(books);
 
             
